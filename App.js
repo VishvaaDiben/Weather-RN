58d99d9b87ca7363ec2ab4481da61fc0e0156da4
unknown <dibenvishvaa@gmail.com>
2020-04-11 11:04:55 +0800

    Changelog 1.0.0 :

--- App.js
+++ App.js
@@ -2,21 +2,34 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { apiConfig } from "./apiKeys";
 import DayCard from "./DayCard";
+import TodayCard from "./TodayCard";
+var moment = require("moment");
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       fullData: [],
-      dailyData: []
+      dailyData: [],
+      today: [],
+      isLoading: true,
+      dataLoading: true
     };
   }
 
   componentDidMount = () => {
-    const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig}`;
+    const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=50450&units=metric&APPID=${apiConfig}`;
 
     fetch(weatherURL)
-      .then(res => res.json())
+      //Error handling using promises
+      .then(res => {
+        if (res.status >= 200 && res.status <= 299) {
+          this.setState({ isLoading: false });
+          return res.json();
+        } else {
+          throw Error(res.statusText);
+        }
+      })
       .then(data => {
         const dailyData = data.list.filter(reading =>
           reading.dt_txt.includes("18:00:00")
@@ -24,10 +37,15 @@ export default class App extends Component {
         this.setState(
           {
             fullData: data.list,
-            dailyData: dailyData
+            dailyData: dailyData,
+            today: dailyData[0],
+            dataLoading: false
           },
           () => console.log(this.state)
         );
+        // this.setState(prevState => ({
+        //   value: prevState.value + 1
+        // }));
       });
   };
 
@@ -37,14 +55,26 @@ export default class App extends Component {
     ));
   };
 
+  todayFunction = () => {
+    const newDateReading = this.state.dailyData[0];
+    return <TodayCard reading={newDateReading} />;
+  };
+
   render() {
+    const { isLoading, dataLoading } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Simple Weather App</Text>
-        <h1 className="display-1 jumbotron">5-Day Forecast.</h1>
+        <div>
+          {dataLoading ? (
+            <h5>Fetching Today's Weather</h5>
+          ) : (
+            this.todayFunction()
+          )}
+        </div>
         <h5 className="display-5 text-muted">Kuala Lumpur, MY</h5>
         <div className="row justify-content-center">
-          {this.formatDayCards()}
+          {isLoading ? <h5>Fetching The Weather</h5> : this.formatDayCards()}
         </div>
       </View>
     );
--- TodayCard.js
+++ TodayCard.js
@@ -4,20 +4,20 @@ import "./assets/owfont-master/css/owfont-regular.css";
 var moment = require("moment");
 
 export default function DayCard({ reading }) {
-//Destructure the data and populate
+  //Destructure the data and populate
   let newDate = new Date(); //get current time
-  const weekday = reading.dt * 1000;
-  newDate.setTime(weekday);
+  //   const weekday = reading.dt * 1000;
+  //   newDate.setTime(weekday);
 
-  const imgURL = `owf owf-${reading.weather[0].id} owf-3x`;
+  const imgURL = `owf owf-${reading.weather[0].id} owf-5x`;
 
   return (
     <div className="col-sm-2">
       <div className="card">
         <div className="card-body">
-          <h3 className="card-title">{moment(newDate).format("ddd")}</h3>
+          <h3 className="card-title">Today</h3>
           <p className="text-muted">
-            {moment(newDate).format("Do MMM YYYY, h:mm a")}
+            {moment(newDate).format("ddd, Do MMM YYYY, h:mm a")}
           </p>
           <i className={imgURL}></i>
           <h2>{Math.round(reading.main.temp)} °C</h2>
